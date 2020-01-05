f376b9b52afb2771c0decde053ebd9471507c0fe
issamasri91 <elasriisssam@gmail.com>
2020-01-06 00:13:20 +0100

    Initial commit

---
+++ app/src/main/java/com/issamelasri/moneymanager/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.issamelasri.moneymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread();
+
+
+    }
+}
