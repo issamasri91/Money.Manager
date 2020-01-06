57814f57dc1dc3c82dc2ad51b6b2690fc88e9250
issamasri91 <elasriisssam@gmail.com>
2020-01-06 19:26:35 +0100

    add Splash screen

--- app/src/main/java/com/issamelasri/moneymanager/SplashActivity.java
+++ app/src/main/java/com/issamelasri/moneymanager/SplashActivity.java
@@ -2,6 +2,7 @@ package com.issamelasri.moneymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,7 +11,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Thread thread = new Thread();
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+
+        };
+        thread.start();
 
 
     }
