51dffe9bf9b37d2d1d6d2a172c19b200f24a2b52
issamasri91 <elasriisssam@gmail.com>
2020-01-09 01:33:34 +0100

    new auth firebse google sign in part 1

---
+++ app/src/main/java/com/issamelasri/moneymanager/SignActivity.java
@@ -0,0 +1,17 @@
+package com.issamelasri.moneymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class SignActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+    }
+}
