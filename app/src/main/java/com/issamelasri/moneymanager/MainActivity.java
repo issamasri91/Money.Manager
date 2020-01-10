5775fbd9cfead406a8f36240cfe5dbbb229b8bb6
issamasri91 <elasriisssam@gmail.com>
2020-01-10 01:41:43 +0100

    all alright2

---
+++ app/src/main/java/com/issamelasri/moneymanager/MainActivity.java
@@ -0,0 +1,39 @@
+package com.issamelasri.moneymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView name;
+    private Button signOut;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.userName);
+        signOut = findViewById(R.id.logout);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+
+            }
+        });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user !=null){
+            name.setText("hello"+user.getDisplayName()+",");
+        }
+    }
+}
