5e1c985b3faf4a3e1599a731d80679ebfda185c8
ISSAM <elasriisssam@gmail.com>
2020-01-14 13:02:27 +0100

    th up  date

--- app/src/main/java/com/issamelasri/moneymanager/Main2Activity.java
+++ app/src/main/java/com/issamelasri/moneymanager/Main2Activity.java
@@ -1,26 +1,25 @@
 package com.issamelasri.moneymanager;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
-
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.navigation.NavigationView;
-
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Main2Activity extends AppCompatActivity {
@@ -31,6 +30,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -38,6 +41,19 @@ public class Main2Activity extends AppCompatActivity {
                 .setAction("Action", null).show());
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        //i call here username to nav-header
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.userName2);
+        TextView navUsermail = (TextView) headerView.findViewById(R.id.textViewUser);
+        ImageView navPicture = (ImageView)  headerView.findViewById(R.id.PictureProfile);
+        Bundle b=getIntent().getExtras();
+        assert b != null;
+        String username=b.getString("username");
+        String usermail=b.getString("usermail");
+        String userPicture=b.getString("picture");
+        navUsername.setText(username);
+        navUsermail.setText(usermail);
+        navPicture.setImageDrawable(Drawable.createFromPath(userPicture));
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -48,6 +64,8 @@ public class Main2Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
     @Override
@@ -62,6 +80,8 @@ public class Main2Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+
+
     }
 
 }
--- app/src/main/java/com/issamelasri/moneymanager/MainActivity.java
+++ app/src/main/java/com/issamelasri/moneymanager/MainActivity.java
@@ -1,17 +1,24 @@
 package com.issamelasri.moneymanager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.net.DatagramPacket;
+
 public class MainActivity extends AppCompatActivity {
     public TextView name;
     public Button signOut;
+    public TextView mail;
+    public ImageView photoUrl;
+
 
 
 
@@ -20,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        photoUrl = findViewById(R.id.imageView3);
+        mail =findViewById(R.id.userMail);
         name = findViewById(R.id.userName);
         signOut = findViewById(R.id.logout);
         signOut.setOnClickListener(v -> {
@@ -30,13 +39,22 @@ public class MainActivity extends AppCompatActivity {
         });
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
+
+
             name.setText(user.getDisplayName());
+            mail.setText(user.getEmail());
+
+
 
         }
 
     }
     public void btnNext(View v){
         Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        Bundle b = new Bundle();
+        b.putString("username",name.getText().toString());
+        b.putString("usermail",mail.getText().toString());
+        intent.putExtras(b);
         startActivity(intent);
     }
 
