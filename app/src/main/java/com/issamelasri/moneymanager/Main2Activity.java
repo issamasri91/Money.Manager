5df4671b851271a0eea8dd7a2bab5b4731b558b4
ISSAM <elasriisssam@gmail.com>
2020-01-20 13:14:23 +0100

    up date

--- app/src/main/java/com/issamelasri/moneymanager/Main2Activity.java
+++ app/src/main/java/com/issamelasri/moneymanager/Main2Activity.java
@@ -1,14 +1,12 @@
 package com.issamelasri.moneymanager;
 
-import android.app.DatePickerDialog;
-import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -16,14 +14,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,7 +31,7 @@ public class Main2Activity extends AppCompatActivity {
 
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    long time;
     TextView click_me;
     String monthYearStr;
 
@@ -48,16 +44,11 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
-
-
-
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -94,8 +85,8 @@ public class Main2Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        click_me = findViewById(R.id.click_me);
 
+        click_me = findViewById(R.id.click_me);
         click_me.setOnClickListener(v -> {
             PickerMonth pickerDialog = new PickerMonth();
             pickerDialog.setListener((datePicker, year, month, i2) -> {
@@ -113,6 +104,7 @@ public class Main2Activity extends AppCompatActivity {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        assert date != null;
         return sdf.format(date);
 
     }
@@ -124,8 +116,20 @@ public class Main2Activity extends AppCompatActivity {
         return true;
 
 
-    }
 
+    }
+@Override
+public boolean onOptionsItemSelected(MenuItem item){
+    int id = item.getItemId();
+
+    if (id == R.id.action_settings) {
+        super.onBackPressed();
+            Toast.makeText(Main2Activity.this, "sign out", Toast.LENGTH_LONG).show();
+
+
+        return true;
+    }return super.onOptionsItemSelected(item);
+}
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -135,6 +139,13 @@ public class Main2Activity extends AppCompatActivity {
 
     }
 
-
+    @Override
+    public void onBackPressed(){
+        if (time+ 2000 > System.currentTimeMillis()){
+            super.onBackPressed();
+        }else {
+            Toast.makeText(Main2Activity.this,"Press to exit",Toast.LENGTH_SHORT).show();
+        }time=System.currentTimeMillis();
+    }
 
 }
